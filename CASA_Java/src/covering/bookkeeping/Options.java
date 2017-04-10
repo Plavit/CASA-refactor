4246d8f5cfa35080ad4314fbc597e889868bca81
kohouj13 <kohouj13@fel.cvut.cz>
2017-04-10 12:01:59 +0200

    covering/bookkeeping/Options

--- CASA_Java/src/covering/bookkeeping/Options.java
+++ CASA_Java/src/covering/bookkeeping/Options.java
@@ -17,6 +17,9 @@ package covering.bookkeeping;
 // You should have received a copy of the GNU General Public License
 // along with CASA.  If not, see <http://www.gnu.org/licenses/>.
 
+import jdk.internal.org.objectweb.asm.tree.IincInsnNode;
+
+import java.util.Random;
 import java.util.Vector;
 
 public class Options {
@@ -25,50 +28,98 @@ public class Options {
     protected Vector<Integer> owningOptions;
 
     public Options() {
-        //TODO
+        cumulativeValueCounts = new Vector<>();
+        owningOptions = new Vector<>();
     }
 
-    public Options(Vector<Integer> valueCounts) {
-        //TODO
+    public Options(Vector<Integer> values) {
+        cumulativeValueCounts = new Vector<>(values.size());
+        int cumulativeValueCount = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            cumulativeValueCount += values.get(i);
+            cumulativeValueCounts.set(i, cumulativeValueCount);
+        }
+        owningOptions = new Vector<>(cumulativeValueCount);
+        for (int i = 0, j = 0; i < values.size(); ++i) {
+            for (int k = values.get(i); k > 0; k--) {
+                owningOptions.set(j++, i);
+            }
+        }
     }
 
     public Options(Options copy) {
-        //TODO
+        cumulativeValueCounts = new Vector<>(copy.getCumulativeValueCounts());
+        owningOptions = new Vector<>(copy.getOwningOptions());
     }
 
     public Integer getSize() {
-        //TODO
+        return cumulativeValueCounts.size();
     }
 
     public Integer getFirstSymbol(Integer option) {
-        //TODO
+        return option > 0 ? cumulativeValueCounts.get(option - 1) : 0;
     }
 
     public Vector<Integer> getFirstSymbols() {
-        //TODO
+        int size = cumulativeValueCounts.size();
+        Vector<Integer> result = new Vector<>(size);
+        for (int i = size; i > 1; i--) {
+            result.set(i, cumulativeValueCounts.get(i - 1));
+        }
+        if (size > 0) {
+            result.set(0, 0);
+        }
+        return result;
     }
 
     public Integer getSymbolCount(Integer option) {
-        //TODO
+        return option != 0 ?
+                (cumulativeValueCounts.get(option) - cumulativeValueCounts.get(option - 1)) :
+                cumulativeValueCounts.get(0);
     }
 
     public Vector<Integer> getSymbolCounts() {
-        //TODO
+        int size = cumulativeValueCounts.size();
+        Vector<Integer> result = new Vector<>(size);
+        for (int i = size; i > 1; i--) {
+            result.set(i, cumulativeValueCounts.get(i) - cumulativeValueCounts.get(i - 1));
+        }
+        if (size > 0) {
+            result.set(0, cumulativeValueCounts.get(0));
+        }
+        return result;
     }
 
     public Integer getLastSymbol(Integer option) {
-        //TODO
+        return cumulativeValueCounts.get(option) - 1;
     }
 
     public Integer getRandomSymbol(Integer option) {
-        //TODO
+        Random rand = new Random();
+        int randomNum = rand.nextInt(Integer.MAX_VALUE);
+        return randomNum % getSymbolCount(option) + getFirstSymbol(option);
     }
 
     public Integer getOtherRandomSymbol(Integer option, Integer exclude) {
-        //TODO
+        int count = getSymbolCount(option);
+        if (count == 1) {
+            return getFirstSymbol(option);
+        }
+        Random rand = new Random();
+        int randomNum = rand.nextInt(Integer.MAX_VALUE);
+        int result = randomNum % (count - 1) + getFirstSymbol(option);
+        return (result >= exclude) ? (result + 1) : result;
     }
 
     public Integer getOption(Integer symbol) {
-        //TODO
+        return owningOptions.get(symbol);
+    }
+
+    public Vector<Integer> getCumulativeValueCounts() {
+        return cumulativeValueCounts;
+    }
+
+    public Vector<Integer> getOwningOptions() {
+        return owningOptions;
     }
 }
