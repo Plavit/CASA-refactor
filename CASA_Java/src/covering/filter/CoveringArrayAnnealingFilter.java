dd83c1655d765e757822071c1c221886ebe04aa9
kohouj13 <kohouj13@fel.cvut.cz>
2017-04-17 13:19:28 +0200

    covering/filter

---
+++ CASA_Java/src/covering/filter/CoveringArrayAnnealingFilter.java
@@ -0,0 +1,41 @@
+package covering.filter;
+
+// Copyright 2008, 2009 Brady J. Garvin
+
+// This file is part of Covering Arrays by Simulated Annealing (CASA).
+
+// CASA is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CASA is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CASA.  If not, see <http://www.gnu.org/licenses/>.
+
+import annealing.AnnealingFilter;
+import covering.cost.CoverageCost;
+import covering.state.CoveringArray;
+
+//TODO need AnnealingFilter
+public class CoveringArrayAnnealingFilter extends AnnealingFilter<CoveringArray, CoverageCost> {
+
+    public static final double MULTIPLE_COVERAGE_WEIGHT = 0.1;
+
+    public CoveringArrayAnnealingFilter(double temperature, double decay) {
+        super(temperature, decay);
+    }
+
+    public double convertToDelta(CoverageCost childEstimate, CoverageCost parentEstimate) {
+        long childNoncoverage = childEstimate.getNoncoverage();
+        long parentNoncoverage = parentEstimate.getNoncoverage();
+        long childMultipleCoverage = childEstimate.getMultipleCoverage();
+        long parentMultipleCoverage = parentEstimate.getMultipleCoverage();
+        return -(double) (childNoncoverage - parentNoncoverage) - MULTIPLE_COVERAGE_WEIGHT
+                * (childMultipleCoverage - parentMultipleCoverage);
+    }
+}
